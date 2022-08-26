c6eebfd5cbc07abf58b9c152a101ca6b4d0ce414
김도현 <xdswwwj@gmail.com>
2022-08-26 21:10:56 +0900

    router 적용

--- ts-social-media-website/src/App.tsx
+++ ts-social-media-website/src/App.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import "./App.css";
-
-interface UserInterface {
-  name: string;
-  age: number;
-  email?: string; // null 허용
-}
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
 
 const App = () => {
   return (
-    <div className="App">
-      <User name="Dohyun" age={30} email="dev-do@naver.com" />
-      <User name="Hoyoung" age={31} email="Hoyoung@naver.com" />
-      <User name="경우의 수" age={50} />
-    </div>
-  );
-};
-
-const User = (props: UserInterface) => {
-  const { name, age, email } = props;
-  console.log(props);
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{age}</h2>
-      <h2>{email}</h2>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
   );
 };
 
--- ts-social-media-website/src/index.tsx
+++ ts-social-media-website/src/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
