d71906301c368d962b3c496a5cf50f3501067a6a
김도현 <xdswwwj@gmail.com>
2022-08-29 00:00:09 +0900

    layout 추가

---
+++ ts-social-media-website/src/layouts/common/wrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const wrapper = () => {
+  return <div></div>;
+};
+
+export default wrapper;
---
+++ ts-social-media-website/src/layouts/template/commonWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const commonWrapper = () => {
+  return <div></div>;
+};
+
+export default commonWrapper;
